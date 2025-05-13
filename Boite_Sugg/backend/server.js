a02cc2667fe29c5511aff295cfe528ecf765bdc1
haydenprod <detygonto@gmail.com>
2025-05-13 18:55:02 +0000

    change env variable

--- Boite_Sugg/backend/server.js
+++ Boite_Sugg/backend/server.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 // Middleware
 app.use(cors());
